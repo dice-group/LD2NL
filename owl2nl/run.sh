09f5143e068b4aea9469052ea0ae4a9b41ebd276
Rene Speck <speckrene@gmail.com>
2020-12-16 19:44:26 +0100

    Adds examples.

--- owl2nl/run.sh
+++ owl2nl/run.sh
@@ -3,7 +3,8 @@
 export MAVEN_OPTS="-Xmx8G"
 
 mainClass="org.aksw.owl2nl.raki.Pipeline"
-axioms="/path/to/owl/file/axioms.owl"
-output="/path/to/owl/file/axioms.json"
+axioms="examples/exampleA.owl"
+output="examples/exampleA.json"
+log="example.log"
 
-nohup mvn exec:java  -Dexec.mainClass=$mainClass -Dexec.args="$axioms $output" > main.log &
+nohup mvn exec:java  -Dexec.mainClass=$mainClass -Dexec.args="$axioms $output" > $log &
