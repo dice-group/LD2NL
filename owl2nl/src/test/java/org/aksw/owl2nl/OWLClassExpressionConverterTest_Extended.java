2ac6411ad4ed1a66afa98c0dde534630a9f5d401
bonvamby <ambuj.mondal@outlook.com>
2020-12-23 01:49:05 +0100

    Created new test file

---
+++ owl2nl/src/test/java/org/aksw/owl2nl/OWLClassExpressionConverterTest_Extended.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * OWL2NL
+ * %%
+ * Copyright (C) 2015 Agile Knowledge Engineering and Semantic Web (AKSW)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * 
+ */
+package org.aksw.owl2nl;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.semanticweb.owlapi.dlsyntax.renderer.DLSyntaxObjectRenderer;
+import org.semanticweb.owlapi.io.ToStringRenderer;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.DefaultPrefixManager;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
+
+
+/**
+ * @author Lorenz Buehmann
+ *
+ */
+public class OWLClassExpressionConverterTest_Extended {
+
+	private static OWLClassExpressionConverter converter;
+
+	private static OWLClass place;
+	private static OWLClass company;
+	private static OWLClass person;
+	private static OWLDataFactoryImpl df;
+	private static OWLNamedIndividual paderborn;
+
+	private static OWLObjectProperty workPlace;
+
+	OWLClassExpression ce;
+	String text;
+
+	/**
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		converter = new OWLClassExpressionConverter();
+
+		df = new OWLDataFactoryImpl();
+		PrefixManager pm = new DefaultPrefixManager("http://dbpedia.org/ontology/");
+
+
+		place = df.getOWLClass("Place", pm);
+		company = df.getOWLClass("Company", pm);
+		person = df.getOWLClass("Person", pm);
+
+		workPlace = df.getOWLObjectProperty("workPlace", pm);
+		paderborn = df.getOWLNamedIndividual("Paderborn", pm);
+
+		ToStringRenderer.getInstance().setRenderer(new DLSyntaxObjectRenderer());
+	}
+
+	@Test
+	public void testhasValue() {
+		// work place is a place and is named paderborn
+		ce = df.getOWLObjectHasValue(workPlace, paderborn);
+		text = converter.convert(ce);
+		System.out.println(ce + " = " + text);
+	}
+
+}
