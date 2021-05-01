aa86816e80181200eef6c257273394c5b37d21ef
Ambuj Mondal <ambuj.mondal@outlook.com>
2021-05-01 13:23:50 +0200

    api code structure

---
+++ api/src/main/java/org/diceresearch/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package org.diceresearch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
