f7954616fdf98cd9c209e0c1b3fb558be92f7210
Rene Speck <speckrene@gmail.com>
2021-07-18 13:18:03 +0200

    Updates LICENSE.

--- owl2nl/src/main/java/org/aksw/owl2nl/ui/OWL2NLCommandLineInterface.java
+++ owl2nl/src/main/java/org/aksw/owl2nl/ui/OWL2NLCommandLineInterface.java
@@ -1,3 +1,23 @@
+/*-
+ * #%L
+ * OWL2NL
+ * %%
+ * Copyright (C) 2015 - 2021 Data and Web Science Research Group (DICE)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 package org.aksw.owl2nl.ui;
 
 import java.util.HashMap;
@@ -16,12 +36,6 @@ import gnu.getopt.Getopt;
 
 public class OWL2NLCommandLineInterface {
   protected static final Logger LOG = LogManager.getLogger(OWL2NLCommandLineInterface.class);
-
-  /**
-   *
-   * @param args
-   * @throws IllegalArgumentException
-   */
   public static void main(final String[] args) throws IllegalArgumentException {
 
     // "https://its-wiki.no/images/0/05/Travel.owl";
