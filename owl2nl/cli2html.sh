3368cbe163e272cc7822b35b7f9f2e698666ae9d
Rene Speck <speckrene@gmail.com>
2021-07-22 12:00:10 +0200

    use batch mode.

--- owl2nl/cli2html.sh
+++ owl2nl/cli2html.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # input parameters
 i="https://protege.stanford.edu/ontologies/travel.owl"
-#i="http://www.ling.helsinki.fi/kit/2004k/ctl310semw/Protege/koala.owl"
+# i="http://www.ling.helsinki.fi/kit/2004k/ctl310semw/Protege/koala.owl"
 # i="/media/store/travel.owl"
 o="eval.html"
 u="true" # true for URL, false for File
@@ -13,7 +13,7 @@ args="-i$i -o$o -u$u"
 
 export MAVEN_OPTS="-Xmx4G"
 
-nohup mvn exec:java  \
+nohup mvn exec:java -B \
 	-Dexec.args="$args" \
 	-Dexec.mainClass=$mainClass \
 	> $0.log 2>&1 </dev/null &
