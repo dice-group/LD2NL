6c9c9d6008a909e9d60e38ea49f66f33a3c96959
Rene Speck <speckrene@gmail.com>
2021-07-27 15:32:59 +0200

    Updates logger.

--- SPARQL2NL/src/main/java/org/aksw/sparql2nl/naturallanguagegeneration/WordTypeDetector.java
+++ SPARQL2NL/src/main/java/org/aksw/sparql2nl/naturallanguagegeneration/WordTypeDetector.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -32,91 +32,91 @@ import net.sf.extjwnl.data.Synset;
 import net.sf.extjwnl.dictionary.Dictionary;
 
 public class WordTypeDetector {
-	
-	private static final Logger logger = LoggerFactory.getLogger(WordTypeDetector.class);
-	
-	public Dictionary dict;	
-	
-	private static final double THRESHOLD = 0.5;
-	private boolean stemWords = true;
-	
-	public WordTypeDetector() {
-		try {
-			dict = Dictionary.getDefaultResourceInstance();
-		} catch (JWNLException e) {
-			logger.error("Failed to load WordNet.", e);
-		}
-	}
-	
-	public boolean isNoun(String keyword) {
-		System.out.println(keyword);
-		try {
-			String token = getFirstToken(keyword);
-			
-			int nrOfNounSenses = 0;
-			int nrOfVerbSenses = 0;
-			
-			// get NOUN senses
-			IndexWord iw;
-			if(stemWords){
-				iw = dict.getMorphologicalProcessor().lookupBaseForm(POS.NOUN, token);
-			} else {
-				iw = dict.getIndexWord(POS.NOUN, token);
-			}
-			if(iw != null){
-				List<Synset> synsets = iw.getSenses();
-				nrOfNounSenses = synsets.size();
-			}
-			//get VERB senses
-			if(stemWords){
-				iw = dict.getMorphologicalProcessor().lookupBaseForm(POS.VERB, token);
-			} else {
-				iw = dict.getIndexWord(POS.VERB, token);
-			}
-			System.out.println("#Nouns: " + nrOfNounSenses);
-			System.out.println("#Verbs: " + nrOfVerbSenses);
-			
-			double score = Math.abs(Math.log((double)nrOfVerbSenses/nrOfNounSenses));
-			System.out.println("Score: " + score);
-			if(score > THRESHOLD){
-				return true;
-			}
-			
-			/*
-			if(nrOfNounSenses == 0 && nrOfVerbSenses != 0){
-				return false;
-			} else if(nrOfNounSenses != 0 && nrOfVerbSenses == 0){
-				return true;
-			} else if(nrOfNounSenses == 0 && nrOfVerbSenses == 0){
-				return false;
-			} else {
-				if((double)nrOfNounSenses/nrOfVerbSenses < THRESHOLD){
-					return false;
-				} else if((double)nrOfVerbSenses/nrOfNounSenses < THRESHOLD){
-					return true;
-				}
-			}*/
-			
-			
-		} catch (JWNLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	private String getFirstToken(String phrase){
-		String[] tokens = phrase.split(" ");
-        return tokens[0];
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(new WordTypeDetector().isNoun("is"));
-		System.out.println(new WordTypeDetector().isNoun("is part of"));
-		System.out.println(new WordTypeDetector().isNoun("population total"));
-		System.out.println(new WordTypeDetector().isNoun("star"));
-		System.out.println(new WordTypeDetector().isNoun("starring"));
-		System.out.println(new WordTypeDetector().isNoun("award"));
-		System.out.println(new WordTypeDetector().isNoun("key"));
-		System.out.println(new WordTypeDetector().isNoun("spouse"));
-	}
+
+  private static final Logger logger = LoggerFactory.getLogger(WordTypeDetector.class);
+
+  public Dictionary dict;
+
+  private static final double THRESHOLD = 0.5;
+  private final boolean stemWords = true;
+
+  public WordTypeDetector() {
+    try {
+      dict = Dictionary.getDefaultResourceInstance();
+    } catch (final JWNLException e) {
+      logger.error("Failed to load WordNet.", e);
+    }
+  }
+
+  public boolean isNoun(final String keyword) {
+    logger.debug(keyword);
+    try {
+      final String token = getFirstToken(keyword);
+
+      int nrOfNounSenses = 0;
+      final int nrOfVerbSenses = 0;
+
+      // get NOUN senses
+      IndexWord iw;
+      if (stemWords) {
+        iw = dict.getMorphologicalProcessor().lookupBaseForm(POS.NOUN, token);
+      } else {
+        iw = dict.getIndexWord(POS.NOUN, token);
+      }
+      if (iw != null) {
+        final List<Synset> synsets = iw.getSenses();
+        nrOfNounSenses = synsets.size();
+      }
+      // get VERB senses
+      if (stemWords) {
+        iw = dict.getMorphologicalProcessor().lookupBaseForm(POS.VERB, token);
+      } else {
+        iw = dict.getIndexWord(POS.VERB, token);
+      }
+      logger.debug("#Nouns: " + nrOfNounSenses);
+      logger.debug("#Verbs: " + nrOfVerbSenses);
+
+      final double score = Math.abs(Math.log((double) nrOfVerbSenses / nrOfNounSenses));
+      logger.debug("Score: " + score);
+      if (score > THRESHOLD) {
+        return true;
+      }
+      /**
+       * <code>
+       if (nrOfNounSenses == 0 && nrOfVerbSenses != 0) {
+         return false;
+       } else if (nrOfNounSenses != 0 && nrOfVerbSenses == 0) {
+         return true;
+       } else if (nrOfNounSenses == 0 && nrOfVerbSenses == 0) {
+         return false;
+       } else {
+         if ((double) nrOfNounSenses / nrOfVerbSenses < THRESHOLD) {
+           return false;
+         } else if ((double) nrOfVerbSenses / nrOfNounSenses < THRESHOLD) {
+           return true;
+         }
+       }
+      </code>
+       */
+    } catch (final JWNLException e) {
+      logger.error(e.getLocalizedMessage(), e);
+    }
+    return false;
+  }
+
+  private String getFirstToken(final String phrase) {
+    final String[] tokens = phrase.split(" ");
+    return tokens[0];
+  }
+
+  public static void main(final String[] args) {
+    System.out.println(new WordTypeDetector().isNoun("is"));
+    System.out.println(new WordTypeDetector().isNoun("is part of"));
+    System.out.println(new WordTypeDetector().isNoun("population total"));
+    System.out.println(new WordTypeDetector().isNoun("star"));
+    System.out.println(new WordTypeDetector().isNoun("starring"));
+    System.out.println(new WordTypeDetector().isNoun("award"));
+    System.out.println(new WordTypeDetector().isNoun("key"));
+    System.out.println(new WordTypeDetector().isNoun("spouse"));
+  }
 }
