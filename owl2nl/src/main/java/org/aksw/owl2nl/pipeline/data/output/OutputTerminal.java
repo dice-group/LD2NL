84a149be6365536f58707c70e6989e029a0f189c
renespeck <speckrene@gmail.com>
2023-10-27 17:22:45 +0200

    Fix.

--- owl2nl/src/main/java/org/aksw/owl2nl/pipeline/data/output/OutputTerminal.java
+++ owl2nl/src/main/java/org/aksw/owl2nl/pipeline/data/output/OutputTerminal.java
@@ -2,7 +2,6 @@ package org.aksw.owl2nl.pipeline.data.output;
 
 import java.util.Map;
 import java.util.Map.Entry;
-
 import org.semanticweb.owlapi.model.OWLAxiom;
 
 /**
@@ -12,18 +11,17 @@ import org.semanticweb.owlapi.model.OWLAxiom;
  *
  */
 public class OutputTerminal implements IOutput<String> {
-  final StringBuilder sb = new StringBuilder();
+  StringBuilder sb = new StringBuilder();
 
   /**
    * Writes to the log and returns true.
    */
   @Override
   public String write(final Map<OWLAxiom, String> verb) {
-    final StringBuilder sb = new StringBuilder();
+    sb = new StringBuilder();
     for (final Entry<OWLAxiom, String> entry : verb.entrySet()) {
       sb.append(entry.getKey()).append(" ").append(entry.getValue()).append(System.lineSeparator());
     }
-    LOG.info(sb);
     return sb.toString();
   }
 
