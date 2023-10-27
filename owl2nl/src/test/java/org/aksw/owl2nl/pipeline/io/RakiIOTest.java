852ae7d1e5286377430b993e355966f11970f39e
renespeck <speckrene@gmail.com>
2023-10-27 15:15:41 +0200

    Adds test.

---
+++ owl2nl/src/test/java/org/aksw/owl2nl/pipeline/io/RakiIOTest.java
@@ -0,0 +1,22 @@
+package org.aksw.owl2nl.pipeline.io;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.IRI;
+
+public class RakiIOTest {
+  protected static final Logger LOG = LogManager.getLogger(RakiIOTest.class);
+
+  @Test
+  public void test() {
+    Model model = RakiIO.readRDFXML(//
+        IRI.create(RakiIOTest.class.getClassLoader().getResource("test.owl")//
+        ).toURI().getPath());
+
+    Assert.assertNotNull(model);
+    Assert.assertFalse(model.isEmpty());
+  }
+}
