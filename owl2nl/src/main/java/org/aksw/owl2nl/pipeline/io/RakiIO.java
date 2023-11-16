043c08b6fa54191fbb65630c53461b03617f3b89
renespeck <speckrene@gmail.com>
2023-11-16 16:35:36 +0100

    Checks input for RDF/XML format since OWL format is not supported can be achieved by for instance transformation with protege.

--- owl2nl/src/main/java/org/aksw/owl2nl/pipeline/io/RakiIO.java
+++ owl2nl/src/main/java/org/aksw/owl2nl/pipeline/io/RakiIO.java
@@ -1,14 +1,10 @@
 package org.aksw.owl2nl.pipeline.io;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.Lang;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,14 +17,23 @@ public class RakiIO {
   protected static final Logger LOG = LogManager.getLogger(RakiIO.class);
 
   public static synchronized Model readRDFXML(final String file) {
-    try { 
-      return ModelFactory.createDefaultModel().read(
-          new ByteArrayInputStream(Files.readAllBytes(Paths.get(file))), Lang.RDFXML.getName());
-    } catch (final IOException e) {
+    Model model = null;
+    boolean isRDF = false;
+    try {
+      LOG.debug(file);
+      model = ModelFactory.createDefaultModel().read(file);
+      isRDF = model.getNsPrefixURI("rdf") != null;
+
+    } catch (final Exception e) {
       LOG.error("Could not read: " + file);
       LOG.error(e.getLocalizedMessage(), e);
-      return null;
+
     }
+    if (!isRDF) {
+      throw new IllegalArgumentException(
+          "\n\n-> !! The given input seem to be not in RDF/XML format!!\n\n");
+    }
+    return model;
   }
 
   public static synchronized boolean write(final Path file, final byte[] bytes) {
